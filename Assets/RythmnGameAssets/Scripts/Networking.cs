d5a3427b69029206e6bcd5c42c7fcae594f69935
evanrg <evangearhart@icloud.com>
2026-03-15 14:10:31 -0400

    Basic packet setup. Instruments can now connect and display on the scoreboard when running. Will need to change instrument once we have selection implemented.

--- Assets/RythmnGameAssets/Scripts/Networking.cs
+++ Assets/RythmnGameAssets/Scripts/Networking.cs
@@ -1,6 +1,64 @@
+using Newtonsoft.Json;
+
 using UnityEngine;
 
 using NativeWebSocket;
+using System;
+
+public enum Sender
+{
+    CLIENT,
+    SCOREBOARD,
+    SERVER
+}
+
+public enum PacketType
+{
+    CONNECTION,
+    SCORE,
+}
+
+public class WebPacket
+{
+    public Sender Sender;
+    public PacketType Type;
+
+    public WebPacket(Sender sender, PacketType type)
+    {
+        Sender = sender;
+        Type = type;
+    }
+}
+
+public enum Instrument
+{
+    BASS,
+    GUITAR,
+    KEYS,
+    VOCALS
+}
+
+public class ConnectionPacket : WebPacket
+{
+    public Instrument Instrument;
+    public long TimeSent;
+
+    public ConnectionPacket(Instrument instrument) : base(Sender.CLIENT, PacketType.CONNECTION)
+    {
+        Instrument = instrument;
+        TimeSent = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+    }
+
+    public string ToJSON()
+    {
+        return JsonConvert.SerializeObject(this);
+    }
+
+    public static ConnectionPacket FromJSON(String jsonString)
+    {
+        return JsonConvert.DeserializeObject<ConnectionPacket>(jsonString);
+    }
+}
 
 public class Networking : MonoBehaviour
 {
@@ -12,6 +70,7 @@ public class Networking : MonoBehaviour
 
         websocket.OnOpen += () => {
             Debug.Log("Web socket connection established.");
+            SendConnectionPacket();
         };
 
         websocket.OnClose += (e) => {
@@ -20,10 +79,22 @@ public class Networking : MonoBehaviour
 
         websocket.OnMessage += (bytes) => {
             Debug.Log("Message received:");
-            Debug.Log(bytes);
+            string message = System.Text.Encoding.Default.GetString(bytes);
+            Debug.Log(message);
         };
 
         await websocket.Connect();
+    }
+
+    async void SendConnectionPacket()
+    {
+        if (websocket.State == WebSocketState.Open)
+        {
+            // TODO: Change this later to be selected instrument instead
+            // of bass default
+            ConnectionPacket cPacket = new(Instrument.BASS);
+            await websocket.SendText(cPacket.ToJSON());
+        }
     }
 
     void Update()
