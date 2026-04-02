f577f73692d0d38702e13f8920acbad61b355475
Tori R <vmr7783@rit.edu>
2026-04-02 12:16:46 -0400

    MainMenu start

---
+++ Assets/MainMenu/SelectedSettings.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using RhythmGameAssets.Scripts;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MainMenu
+{
+    public class SelectedSettings : MonoBehaviour
+    {
+        public string Instrument { get; set; }
+        public string Difficulty { get; set; }
+        public Song SelectedSong { get; set; }
+        // TODO update chart parser
+        public Dictionary<string, Song> SongList { get; private set; }
+        [SerializeField] private string songsDir = @"Assets/RhythmGameAssets/Resources/Songs";
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            SongList = new Dictionary<string, Song>();
+            CreateSongList();
+            DefaultSelection(); // TODO remove debug function
+        }
+        
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void CreateSongList()
+        {
+            songsDir = Path.GetFullPath(songsDir);
+            var songs = Directory.GetDirectories(songsDir);
+            // Songs come from the Songs directory
+            foreach (var s in songs)
+            {
+                Song song = new Song(s);
+                // Key: Song Name, Value: Song object
+                SongList.Add(s, song);
+            }
+            // Song mirage = new Song("Mirage");
+            // SongList.Add(mirage);
+        }
+
+        private void DefaultSelection()
+        {
+            SelectedSong = SongList["Mirage"];
+        }
+    }
+}
